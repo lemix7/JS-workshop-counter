455f736abfa6ff6b7b38a356a2bc941d4c303533
Ahmad <ahmedgeger@gmail.com>
2024-12-09 01:09:54 +0200

    add reset btn

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # JS-workshop-counter
+# JS-workshop-counter
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let count = document.querySelector('.count') 
 let incrementBtn = document.getElementById('increment')
 let decrementBtn = document.getElementById('decrement')
+let resetBtn = document.getElementById('reset')
 
 let countValue = 0
 
@@ -16,7 +17,12 @@ function decrement () {
     count.innerText = countValue
 }
 
+function reset() {
+    countValue = 0
+    count.innerText = countValue
+}
 
 
 incrementBtn.addEventListener('click',increment)
 decrementBtn.addEventListener('click',decrement) 
+resetBtn.addEventListener('click',reset)
